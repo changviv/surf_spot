a80a2a195a9d3e479a643c6e3ba7992048065858
Charlie Paull <charliepaull211@gmail.com>
2019-07-14 15:04:19 -0700

    rebaseing

---
+++ surfspot/src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar () {
+    return (
+        <div>
+        <title>Surf Spot</title>
+            <h1>Location</h1>
+        </div>
+    )
+}
+
+export default NavBar;
