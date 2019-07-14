201df89c94dbbfffa11b208a6a7cb539d0f716bf
Charlie Paull <charliepaull211@gmail.com>
2019-07-14 14:21:16 -0700

    add to app & stuff.

---
+++ surfspot/src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import Nav from "./components/Nav";
+import Jumbo from "./components/Jumbotron";
+
+class App extends Component {
+    state = {
+        
+    }
+} 
+
+export default App;
