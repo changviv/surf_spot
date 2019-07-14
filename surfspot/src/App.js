a0f455d982aa0fb31b0a6ff085d5be36bbf051a2
Charlie Paull <charliepaull211@gmail.com>
2019-07-14 14:01:30 -0700

    more files

--- surfspot/src/App.js
+++ surfspot/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
-// import Nav from "./components/Nav";
-// import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 import SearchBar from './components/SearchBar';
 
 class App extends Component {
