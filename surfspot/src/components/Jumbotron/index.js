6d5754ea5523edea89185537b293c86389cb0a94
Charlie Paull <charliepaull211@gmail.com>
2019-07-21 12:36:48 -0700

    jumbo formatting

--- surfspot/src/components/Jumbotron/index.js
+++ surfspot/src/components/Jumbotron/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Container, Divider } from 'semantic-ui-react'
 
-const ContainerExampleAlignment = () => (
+const jumboStyle = {
+  boxSizing: "border-box",
+  width: "100%",
+  border: "solid #5B6DCD 10px",
+  padding: "5px"
+}
+
+const ContainerExampleAlignment = (props) => (
   <div>
-    <Container textAlign='justified'>
+    <Container style={jumboStyle} textAlign='justified'>
       {/* <b>Justified</b> */}
+      {props.children}
       <Divider />
       <p>
-        Hello, this is Surf Spot. Find your ideal shredding conditions below! Type in a Location
+        Hello, this is Surf Spot. Find your ideal shredding conditions below! Type in a Location.
       </p>
     </Container>
   </div>
